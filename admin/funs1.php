66980a1733fddacd87f504fe11c21faa08c6adc2
Kavinda Silva <kss.kavinda@gmail.com>
2017-06-08 11:53:20 +0530

    styles1

--- admin/funs1.php
+++ admin/funs1.php
@@ -1,15 +1,49 @@
 <?php
 
-require_once '../dbcon.php';
+require "../dbcon.php";
+//global $conn;
 
-function viewStudents(){
-	$sqlq="select * from ";
-	$res=mysqli_query($conn, $sqlq);
+//view students list
+function viewStudents() {
+	$sqlq = "select * from student;"; //sql query
+	$res = mysqli_query($GLOBALS['conn'] , $sqlq); //result
 	
+	if (mysqli_num_rows($res) == 0) //check result
+		echo "<p>No students in the table</p>";
 	
+	else {
+		while ($row = mysqli_fetch_array($res)) {
+			echo "<form method='post' action='mail_functions.php'>";
+			echo "<tr><input type='text' name='id' value='" . $row['id'] . "' hidden/>";
+			echo "<td>" . $row['semail'] . "</td>";
+			echo "<td>" . $row['subject'] . "</td>";
+			echo "<td>" . $row['time_sent'] . "</td>";
+			echo "<td><input type='submit' name='view' value='VIEW'/></td>";
+			echo "<td><input type='submit' name='delete' value='DELETE'/></td></tr></form>";
+		}
+	}
 }
 
+//view techers list
+function viewTeachers() {
+	$sqlq = "select * from teacher"; //sql query
+	$res = mysqli_query($GLOBALS['conn'] , $sqlq); //result
+	
+	if (mysqli_num_rows($res) == 0) //check result
+		echo "<p>No teachers in the table</p>";
+	
+	else {
+		while ($row = mysqli_fetch_array($res)) {
+			echo "<form method='post' action='mail_functions.php'>";
+			echo "<tr><input type='text' name='id' value='" . $row['id'] . "' hidden/>";
+			echo "<td>" . $row['semail'] . "</td>";
+			echo "<td>" . $row['subject'] . "</td>";
+			echo "<td>" . $row['time_sent'] . "</td>";
+			echo "<td><input type='submit' name='view' value='VIEW'/></td>";
+			echo "<td><input type='submit' name='delete' value='DELETE'/></td></tr></form>";
+		}
+	}
+}
 
 //unctio
-
 ?>
--- admin/index.php
+++ admin/index.php
@@ -1,19 +1,32 @@
 <?php
 //session maintainence
-
-
 ?>
 
 <!DOCTYPE html>
 <html>
 	<head>
 		<title>Admin control panel</title>
+		<link rel="stylesheet" type="text/css" href="../css/tables.css">
 	</head>
-	
+
 	<body>
-		
-		
-		
-		
+		<div>
+			<!--students list-->
+			<div class="teach">
+				<?php include 'funs1.php';
+					viewStudents();
+				?>
+
+			</div>
+			
+			<!--teachers list-->
+			<div>
+				<?php //include 'funs1.php';
+					viewTeachers();
+				?>
+
+			</div>
+		</div>
+
 	</body>
 </html>
